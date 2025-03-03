b3326bb4b12cae96b6b60ae002c4de7136a675cb
Pedro Airton <pedroairton01@gmail.com>
2025-03-03 15:51:08 -0300

    Abertura do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,68 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'petshop';
+  title: string = 'petshop';
+  public mobile: boolean = false;
+  toggleMobile() {
+    this.mobile === true ? (this.mobile = false) : (this.mobile = true);
+  }
+  public consultasHoje: {
+    usuario: string;
+    servico: string;
+    horario_inicio: string;
+  }[] = [
+    {
+      usuario: 'Robertinho',
+      servico: 'Consulta',
+      horario_inicio: '12h30',
+    },
+    {
+      usuario: 'Jonas',
+      servico: 'Vacina',
+      horario_inicio: '14h30',
+    },
+  ];
+  public totalUsuarios: number = 54;
+  public servicosTotal: {
+    servico: string;
+    total: number;
+  }[] = [
+    {
+      servico: 'Banho',
+      total: 38,
+    },
+    {
+      servico: 'Tosa',
+      total: 31,
+    },
+    {
+      servico: 'Corte de unha',
+      total: 20,
+    },
+    {
+      servico: 'Vacina',
+      total: 15,
+    },
+  ];
+  public animaisTotal: { tipo: string; total: number }[] = [
+    {
+      tipo: 'Gato',
+      total: 56,
+    },
+    {
+      tipo: 'Cachorro',
+      total: 47
+    },
+  ];
 }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  public name: string = 'Usuário';
+  public now: number = new Date().getHours()
+  public message: string;
+  public sidebar: boolean = true
+  constructor(){
+    this.message = this.greeting()
+  }
+  greeting(){
+    if(this.now <= 12 && this.now >= 5){
+      return `Bom dia ${this.name}!`
+    } else if(this.now >= 13 && this.now <= 18){
+      return `Boa tarde ${this.name}!`
+    } else if(this.now >= 19 || this.now <= 4){
+      return `Boa noite ${this.name}!`
+    } else {
+      return `Olá ${this.name}!`
+    }
+  }
+  toggleSidebar(){
+    this.sidebar ? this.sidebar = false : this.sidebar = true
+  }
+}
