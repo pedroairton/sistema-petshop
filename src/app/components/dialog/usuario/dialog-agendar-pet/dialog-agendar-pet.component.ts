7bd3c482eef59e2b5b92e9b280948d61d3b3705f
Pedro Airton <pedroairton01@gmail.com>
2025-07-21 18:42:18 -0300

    feat: Formulário de agendamento concluído

--- src/app/components/dialog/usuario/dialog-agendar-pet/dialog-agendar-pet.component.ts
+++ src/app/components/dialog/usuario/dialog-agendar-pet/dialog-agendar-pet.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormField } from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-dialog-agendar-pet',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormField,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-agendar-pet.component.html',
-  styleUrl: './dialog-agendar-pet.component.scss'
+  styleUrl: './dialog-agendar-pet.component.scss',
 })
 export class DialogAgendarPetComponent {
-
+  form = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    servico: new FormControl('', Validators.required),
+    data_nascimento: new FormControl('', Validators.required),
+    tipo_animal: new FormControl('', Validators.required),
+    raca: new FormControl('', Validators.required),
+  });
 }
