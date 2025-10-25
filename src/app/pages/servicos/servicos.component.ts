effde76edc93b615da6b498f8a54072150218d99
Pedro Airton <pedroairton01@gmail.com>
2025-10-25 16:16:15 -0300

    Página de serviços adicionada

--- src/app/pages/servicos/servicos.component.ts
+++ src/app/pages/servicos/servicos.component.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './servicos.component.scss'
 })
 export class ServicosComponent {
-
+public servicosTotal: {
+    servico: string;
+    total: number;
+    status: string
+  }[] = [
+    {
+      servico: 'Banho',
+      total: 38,
+      status: 'Ativo'
+    },
+    {
+      servico: 'Tosa',
+      total: 31,
+      status: 'Ativo'
+    },
+    {
+      servico: 'Corte de unha',
+      total: 20,
+      status: 'Inativo'
+    },
+    {
+      servico: 'Vacina',
+      total: 15,
+      status: 'Ativo'
+    },
+  ];
 }
