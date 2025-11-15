f593df5ae3eb94f978c2a09780d01bfcb6c845af
Pedro Airton <pedroairton01@gmail.com>
2025-11-15 15:21:45 -0300

    URL da api em prod + alteração dos budgets de bundle + remoção de código inútil do componente principal

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { catchError, Observable, throwError } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:8000';
+  private apiUrl = 'https://api1.srv1109011.hstgr.cloud';
 
   constructor(private http: HttpClient) {}
 
